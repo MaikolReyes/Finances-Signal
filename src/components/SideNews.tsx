14d11752f43d74d3abb517f6a602227f7c64c4d0
Maikol Reyes <Maikol.Reyes@somosrex.com>
2025-09-03 21:35:27 -0300

    add sidenews

--- src/components/SideNews.tsx
+++ src/components/SideNews.tsx
@@ -1,37 +1,88 @@
-import Link from 'next/link';
-import { useRecentArticles } from '../hooks/useRecenArticles';
+"use client";
+//
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+import { useRecentArticles } from "../hooks/useRecenArticles";
+// import { getCdnUrl } from "@/utils/getCdnUrl";
+import Image from "next/image";
+import Link from "next/link";
+import { getCdnUrl } from "@/utils/getCdnUrl";
 
 export const SideNews = () => {
     const recentArticles = useRecentArticles();
 
-    return (
-        <div>
-            {recentArticles.slice(3, 5).map(({ title, id, publishedAt, slug }) => {
+    if (!recentArticles || recentArticles.length === 0) return null;
 
-                const formattedDate = new Intl.DateTimeFormat('es-ES', {
-                    year: 'numeric',
-                    month: 'short',
-                    day: '2-digit'
+    return (
+
+
+        <div className="h-full flex flex-col gap-5 md:gap-3 md:grid-cols-2 w-full md:w-1/2">
+            {/* Primera sección: imágenes + contenido */}
+            {recentArticles.slice(3, 5).map(({ title, cover, id, publishedAt, slug, contenido }) => {
+                const formattedDate = new Intl.DateTimeFormat("es-ES", {
+                    year: "numeric",
+                    month: "short",
+                    day: "2-digit",
                 }).format(new Date(publishedAt));
 
                 return (
 
-                    <div key={id} className='w-2/3'>
-                        <Link href={`/article/${slug}`} className="w-full no-underline text-inherit">
-                            <div className="card w-full">
-                                <div className="card-body">
-                                    <h5 className="truncated-title font-title text-base large-desktop:text-xl">
-                                        {title}
-                                    </h5>
-                                    <p className="card-text"><small className="text-date">{formattedDate}</small></p>
-                                </div>
+                    <div key={id} className="card">
+                        <Link href={`/article/${slug}`}>
+                            <div className="relative w-full">
+                                <Image
+                                    src={getCdnUrl(cover)}
+                                    alt={title}
+                                    className="object-cover rounded-sm w-full aspect-[16/9]"
+                                    width={400}
+                                    height={200}
+                                    priority={true}
+                                    quality={75}
+                                />
                             </div>
                         </Link>
-
+                        <div className="p-3">
+                            <Link href={`/article/${slug}`}>
+                                <h2 className="truncated-title class-title">
+                                    {title}
+                                </h2>
+                            </Link>
+                            <div className="truncated-text class-content">
+                                <BlocksRenderer content={contenido} />
+                            </div>
+                            <p className="date">
+                                {formattedDate}
+                            </p>
+                        </div>
                     </div>
-                )
-            })
+                );
+            })}
+
+            {/* Segunda sección: solo título + fecha */}
+            {
+                recentArticles.slice(5, 10).map(({ title, id, publishedAt, slug }) => {
+                    const formattedDate = new Intl.DateTimeFormat("es-ES", {
+                        year: "numeric",
+                        month: "short",
+                        day: "2-digit",
+                    }).format(new Date(publishedAt));
+
+                    return (
+                        <div key={id} className="card p-5">
+                            <Link
+                                href={`/article/${slug}`}
+                                aria-label={`Leer artículo: ${title}`}>
+                                <h2 className=" class-title">
+                                    {title}
+                                </h2>
+                                <p className="date">
+                                    {formattedDate}
+                                </p>
+                                <span className="sr-only">Leer artículo completo</span>
+                            </Link>
+                        </div>
+                    );
+                })
             }
-        </div>
-    )
-}
+        </div >
+    );
+};
